63595b4adb1be3f1d8104b7f7ba31d8b42a54e92
amit-upadhyay-it <youarefreak1@gmail.com>
2018-01-24 19:09:59 +0530

    update

--- graphs/adjacencylist.py
+++ graphs/adjacencylist.py
@@ -40,15 +40,59 @@ class DiGraph:
             print key, '->', [i.vertex for i in lis]
 
 
+# un-directed graph
+class Graph:
+    # In adjacency list representative I would need a mapper to map the node
+    # value with the list representing the adjacency list (i.e. adjacent nodes)
+    # so I use a dictionary to create data to list mapping
+    def __init__(self, node_dic):
+        self.node_dic = node_dic
+
+    # method to create edge between the src and dest node with specified weight
+    def add_edge(self, src, dest, weight):
+        # since this is un-directed so I need to add for src as well as dest
+        # first creating the edge from src to dest
+        if src in self.node_dic:
+            lis = self.node_dic.get(src)
+            lis.append(Node(dest, weight))
+        else:
+            self.node_dic[src] = [Node(dest, weight)]
+
+        # now creating edge from dest to src
+        if dest in self.node_dic:
+            lis = self.node_dic.get(dest)
+            lis.append(Node(src, weight))
+        else:
+            self.node_dic[dest] = [Node(src, weight)]
+
+    # printing the adjacency list
+    def print_graph(self):
+        for key in self.node_dic:
+            # get the list
+            lis = self.node_dic[key]
+            print key, '->', [i.vertex for i in lis]
+
+
 if __name__ == '__main__':
-    graph = DiGraph(dict())
+    digraph = DiGraph(dict())
+    digraph.add_edge('a', 'b', 1)
+    digraph.add_edge('a', 'd', 1)
+    digraph.add_edge('b', 'a', 3)
+    digraph.add_edge('b', 'c', 1)
+    digraph.add_edge('c', 'b', 1)
+    digraph.add_edge('c', 'd', 5)
+    digraph.add_edge('d', 'c', 1)
+    digraph.add_edge('d', 'a', 6)
+
+    print 'printing the digraph'
+    digraph.print_graph()
+    print
+
+    print 'printing the graph'
+    graph = Graph(dict())
     graph.add_edge('a', 'b', 1)
-    graph.add_edge('a', 'd', 1)
-    graph.add_edge('b', 'a', 3)
     graph.add_edge('b', 'c', 1)
-    graph.add_edge('c', 'b', 1)
-    graph.add_edge('c', 'd', 5)
-    graph.add_edge('d', 'c', 1)
-    graph.add_edge('d', 'a', 6)
+    graph.add_edge('c', 'd', 1)
+    graph.add_edge('d', 'a', 1)
 
     graph.print_graph()
