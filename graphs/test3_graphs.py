545cd20712c7918260280b11c71c37f3cd7ed089
amit-upadhyay-it <youarefreak1@gmail.com>
2018-01-28 11:38:01 +0530

    test3 doesn't work fine for digraphs

---
+++ graphs/test3_graphs.py
@@ -0,0 +1,26 @@
+'''
+    v2    v5
+      \  /
+       v1
+      /  \
+    v3    v4
+      \  /
+       v6
+'''
+
+import graphs
+
+
+if __name__ == '__main__':
+
+    digraph = graphs.DiGraph(dict())
+    # agrs: src, dest, weight
+    digraph.add_edge('v2', 'v1', 1)
+    digraph.add_edge('v5', 'v1', 1)
+    digraph.add_edge('v1', 'v3', 1)
+    digraph.add_edge('v1', 'v4', 1)
+    digraph.add_edge('v3', 'v6', 1)
+    digraph.add_edge('v4', 'v6', 1)
+    digraph.add_edge('v4', 'v3', 1)
+
+    digraph.print_graph()
